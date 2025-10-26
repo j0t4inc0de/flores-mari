ae5cf57868500e421cf962578a9a4ece241cc7ec
jota <juan.erices04@inacapmail.cl>
2025-10-26 00:11:08 -0300

    Header Listo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}",
  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+      colors: {
+        primary: '#f7edf0',
+        secondary: '#f4cbc6',
+        accent: '#F4AFAB',
+        vanilla: '#F4EEA9',
+        icterine: '#F4F482',
+      },
+    },
   },
   plugins: [],
 }
