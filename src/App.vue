a6f2aad19b095f6a798f71a77cbc8f5cbe1a2f6e
jota <juan.erices04@inacapmail.cl>
2025-10-26 00:18:48 -0300

    Icono svg sin derechos de autos

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,11 @@
       <div>
         <a href="https://maps.app.goo.gl/tuxRM2f1JJfqfjtZ9" target="_blank" rel="noopener noreferrer" class="mb-10 inline-flex items-center space-x-2 rounded-full border border-primary bg-primary px-4 py-1.5 text-sm text-accent shadow-sm transition-colors hover:border-icterine hover:bg-secondary hover:text-icterine">
           Santa Fe, Calle 4, 6 
-          <svg class="m-1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
-            <path fill="#48b564" d="M35.76,26.36h0.01c0,0-3.77,5.53-6.94,9.64c-2.74,3.55-3.54,6.59-3.77,8.06	C24.97,44.6,24.53,45,24,45s-0.97-0.4-1.06-0.94c-0.23-1.47-1.03-4.51-3.77-8.06c-0.42-0.55-0.85-1.12-1.28-1.7L28.24,22l8.33-9.88	C37.49,14.05,38,16.21,38,18.5C38,21.4,37.17,24.09,35.76,26.36z"></path><path fill="#fcc60e" d="M28.24,22L17.89,34.3c-2.82-3.78-5.66-7.94-5.66-7.94h0.01c-0.3-0.48-0.57-0.97-0.8-1.48L19.76,15	c-0.79,0.95-1.26,2.17-1.26,3.5c0,3.04,2.46,5.5,5.5,5.5C25.71,24,27.24,23.22,28.24,22z"></path><path fill="#2c85eb" d="M28.4,4.74l-8.57,10.18L13.27,9.2C15.83,6.02,19.69,4,24,4C25.54,4,27.02,4.26,28.4,4.74z"></path><path fill="#ed5748" d="M19.83,14.92L19.76,15l-8.32,9.88C10.52,22.95,10,20.79,10,18.5c0-3.54,1.23-6.79,3.27-9.3	L19.83,14.92z"></path><path fill="#5695f6" d="M28.24,22c0.79-0.95,1.26-2.17,1.26-3.5c0-3.04-2.46-5.5-5.5-5.5c-1.71,0-3.24,0.78-4.24,2L28.4,4.74	c3.59,1.22,6.53,3.91,8.17,7.38L28.24,22z"></path>
+          <svg class="m-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+              <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                  <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/>
+                  <circle cx="12" cy="10" r="3"/>
+              </g>
           </svg>
         </a>
       </div>
@@ -17,7 +20,7 @@
       </h1>
 
       <p class="mt-8 max-w-lg text-lg text-primary">
-        Mi nombre es Marisol, y me dedico a la venta de flores y arreglos florales para toda ocasión. Ofrezco una amplia variedad de flores frescas y hermosas para hacer que tus momentos especiales sean aún más memorables.
+        Mi nombre es Marisol, y me dedico a la venta de Ramos y Arreglos Florales para toda ocasión. Ofrezco una amplia variedad de flores frescas y hermosas para hacer que tus momentos especiales sean aún más memorables.
       </p>
 
       <a href="#" class="mt-10 rounded-lg bg-vanilla px-6 py-3 text-base font-medium text-black transition-transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-icterine focus:ring-offset-2 focus:ring-offset-black">
